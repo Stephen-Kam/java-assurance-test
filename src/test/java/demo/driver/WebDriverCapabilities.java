67576903dd411c65a84639b59735b1162472c8a8
Stephen-Kam <20183211+Stephen-Kam@users.noreply.github.com>
2026-02-13 09:49:09 +0000

    minor commit

--- src/test/java/demo/driver/WebDriverCapabilities.java
+++ src/test/java/demo/driver/WebDriverCapabilities.java
@@ -10,7 +10,6 @@ public class WebDriverCapabilities {
   public ChromeOptions getChromeOptions() {
     ChromeOptions options = new ChromeOptions();
     options.setPageLoadStrategy(PageLoadStrategy.EAGER);
-    options.addArguments("--guest");
     if (WebDriverConfig.getHeadless()) {
       options.addArguments("--headless");
     }
